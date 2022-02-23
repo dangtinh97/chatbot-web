20683bf61495757f76dc0c6c325170dbf8987102
Dang Tinh <>
2022-02-23 22:37:52 +0700

    is https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        if(env('APP_ENV') === 'production')
+        if(isset($_SERVER['HTTPS']))
         {
             URL::forceScheme('https');
         }
