2ad1b4c5b92f3ecbdec650f1a97ab72cf76de228
Dang Tinh <>
2022-07-30 22:50:47 +0700

    update game

--- resources/views/apple-worm.blade.php
+++ resources/views/apple-worm.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 
-<iframe src="https://games.crazygames.com/vi_VN/apple-worm/index.html" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
+<iframe src="https://trochoi.io/apple-worm-g31297#play" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
 @include('incluces.analytics')
 </body>
 </html>
